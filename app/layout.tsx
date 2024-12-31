34a17c66a05ce6217f5da75d6c63d76b5c7c4fee
StephenRivero <stephen.m.rivero@gmail.com>
2024-12-31 15:01:48 +0800

    Add Projects section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,43 @@
-import Header from '@/components/Header'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Header from "@/components/Header";
+import "./globals.css";
+import { Inter } from "next/font/google";
+// import Footer from "@/components/footer";
+// import ThemeSwitch from "@/components/theme-switch";
+// import ThemeContextProvider from "@/context/theme-context";
+// import { Toaster } from "react-hot-toast";
+import ActiveSectionContextProvider from "@/context/ActiveSectionContext";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Stephen Rivero | Front-End Developer | Portfolio Website',
-  description: 'Stephen is a Front-End Developer with 3 years of experience developing web apps using technologies such as Tailwind CSS, Framer Motion, Javascript ES6, React.js, and Next.js',
-}
+  title: "Ricardo | Personal Portfolio",
+  description: "Ricardo is a full-stack developer with 8 years of experience.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
-        <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
-        <div className='bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
-        <Header/>
-        {children}
+    <html lang="en" className="!scroll-smooth">
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+      >
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+
+        {/* <ThemeContextProvider> */}
+          <ActiveSectionContextProvider>
+            <Header />
+            {children}
+            {/* <Footer /> */}
+
+            {/* <Toaster position="top-right" /> */}
+            {/* <ThemeSwitch /> */}
+          </ActiveSectionContextProvider>
+        {/* </ThemeContextProvider> */}
       </body>
     </html>
-  )
+  );
 }
